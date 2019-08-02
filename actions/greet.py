b1ef355e74a5247943169bfb28aab1abf9a87117
Sanchita Saha <sanchitas@icteas.com>
2019-08-02 16:31:51 +0530

    initial commit

--- actions/greet.py
+++ actions/greet.py
@@ -7,3 +7,14 @@ class MyEchoAction(Action):
         print(message)
 
 		
+		
+import requests
+from st2common.runners.base_action import Action
+class MyEchoAction(Action):
+	def run(self, message):
+		resp = requests.get('https://jsonplaceholder.typicode.com/todos/1)
+		if resp.status_code != 200:
+			# This means something went wrong.
+			raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+		for todo_item in resp.json():
+			print('{} {}'.format(todo_item['id'], todo_item['userId']))
