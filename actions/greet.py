020a881c73808a8722765f5a3eab0ac9244cef25
Sanchita Saha <sanchitas@icteas.com>
2019-08-02 16:34:39 +0530

    initial commit

--- actions/greet.py
+++ actions/greet.py
@@ -12,7 +12,7 @@ import requests
 from st2common.runners.base_action import Action
 class MyEchoAction(Action):
 	def run(self, message):
-		resp = requests.get('https://jsonplaceholder.typicode.com/todos/1)
+		resp = requests.get('https://jsonplaceholder.typicode.com/todos/1')
 		if resp.status_code != 200:
 			# This means something went wrong.
 			raise ApiError('GET /tasks/ {}'.format(resp.status_code))
