2f6b1a2e30694a2c908d9cbdc41209e2e3e73a73
Sanchita Saha <sanchitas@icteas.com>
2019-08-02 17:32:34 +0530

    initial commit

--- actions/greet.py
+++ actions/greet.py
@@ -4,5 +4,5 @@ class MyEchoAction(Action):
 	def run(self, message):
 		resp = requests.get('https://jsonplaceholder.typicode.com/todos/1')
 		if resp.status_code == 200:
-		for todo_item in resp.json():
-			print('{} {}'.format(todo_item['id'], todo_item['userId']))
+			for todo_item in resp.json():
+				print('{} {}'.format(todo_item['id'], todo_item['userId']))
