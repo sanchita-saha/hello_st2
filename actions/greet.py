a73e6e5e3090e6bf9f58819336f8586a68ad2c79
Sanchita Saha <sanchitas@icteas.com>
2019-08-02 12:17:55 +0530

    initial commit

--- actions/greet.py
+++ actions/greet.py
@@ -1,2 +1,11 @@
-#!/bin/bash
-print($1+" , StackStorm!")
+import sys
+
+from st2common.runners.base_action import Action
+
+class MyEchoAction(Action):
+    def run(self, message):
+        print(message)
+
+        if message == 'working':
+            return (True, message)
+        return (False, message)
