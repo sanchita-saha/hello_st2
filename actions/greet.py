c797f861a3682d5c72b229e337dccf226f6dd55d
Sanchita Saha <sanchitas@icteas.com>
2019-08-02 15:01:48 +0530

    initial commit

--- actions/greet.py
+++ actions/greet.py
@@ -6,6 +6,6 @@ class MyEchoAction(Action):
     def run(self, message):
         print(message)
 
-        if message == 'working':
+		'''if message == 'working':
             return (True, message)
-        return (False, message)
+        return (False, message)'''
